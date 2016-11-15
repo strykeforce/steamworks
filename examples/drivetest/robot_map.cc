80cdbdc80a99d3c4ba3775c8f6b488a02b45a0c5
Jeff Hutchison <jeff@jeffhutchison.com>
2016-11-15 12:26:24 -0500

    Add grapher interface.

--- examples/drivetest/robot_map.cc
+++ examples/drivetest/robot_map.cc
@@ -2,9 +2,37 @@
 
 #include "WPILib.h"
 
+#include "HostToObject.h"
+#include "robot_config.h"
 #include "swerve/talon_map.h"
 
 namespace sidewinder {
+
+namespace {
+inline JerrysGrapher_DeviceBundle jgdb(byte id, ::CANTalon* t) {
+  JerrysGrapher_DeviceBundle db{JerrysGrapher_DeviceType::Talon, id, t};
+  return db;
+}
+
+void initialize_grapher() {
+  typedef RobotMap rm;
+  std::vector<JerrysGrapher_DeviceBundle> gd;
+  gd.push_back(jgdb(rm::kLeftFrontDrive, rm::swerve_talons->lf_drive));
+  gd.push_back(jgdb(rm::kLeftFrontAzimuth, rm::swerve_talons->lf_azimuth));
+
+  gd.push_back(jgdb(rm::kRightFrontDrive, rm::swerve_talons->rf_drive));
+  gd.push_back(jgdb(rm::kRightFrontAzimuth, rm::swerve_talons->rf_azimuth));
+
+  gd.push_back(jgdb(rm::kLeftRearDrive, rm::swerve_talons->lr_drive));
+  gd.push_back(jgdb(rm::kLeftRearAzimuth, rm::swerve_talons->lr_azimuth));
+
+  gd.push_back(jgdb(rm::kRightRearDrive, rm::swerve_talons->rr_drive));
+  gd.push_back(jgdb(rm::kRightRearAzimuth, rm::swerve_talons->rr_azimuth));
+
+  JerrysGrapher_StartStatusThread(&gd);
+}
+
+} /* namespace */
 
 /** Holds pointers to the 8 swerve drive Talons.  */
 TalonMap* RobotMap::swerve_talons = new TalonMap();
@@ -26,6 +54,10 @@ void RobotMap::Init() {
 
   swerve_talons->rr_drive = new ::CANTalon(Talons::kRightRearDrive);
   swerve_talons->rr_azimuth = new ::CANTalon(Talons::kRightRearAzimuth);
+
+#ifdef GRAPHER
+  initialize_grapher();
+#endif
 }
 
 } /* sidewinder */
