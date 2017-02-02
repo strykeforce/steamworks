355ab583344d8a9b1479aeb374d1cfa3801dbb27
Jeff Hutchison <jeff@jeffhutchison.com>
2017-02-02 12:25:28 -0500

    Fix broken transitive dependency.
    File version.h was including a private include file and therefore wasn't
    working on outside projects that use this library.

--- src/version.cc
+++ src/version.cc
@@ -2,6 +2,13 @@
 
 #include <string>
 
-using namespace sidewinder;
+#include "version_defs.h"
 
+namespace sidewinder {
+static const char* VERSION =
+    "SIDEWINDER_VERSION " SIDEWINDER_VERSION " " SIDEWINDER_COMPILE_DATE
+    " " SIDEWINDER_COMPILE_TIME;
+} /* sidewinder */
+
+using namespace sidewinder;
 std::string GetVersion() { return std::string(VERSION); }
--- src/version.h
+++ src/version.h
@@ -2,12 +2,7 @@
 
 #include <string>
 
-#include "version_defs.h"
-
 namespace sidewinder {
-static const char* VERSION =
-    "SIDEWINDER_VERSION " SIDEWINDER_VERSION " " SIDEWINDER_COMPILE_DATE
-    " " SIDEWINDER_COMPILE_TIME;
 
 std::string GetVersion();
 
