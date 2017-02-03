111cf57d74a915cc42f369f3d7860836eae1a907
Jeff Hutchison <jeff@jeffhutchison.com>
2017-02-03 11:01:02 -0500

    Parameterize serial info, support tests.

--- src/link.cc
+++ src/link.cc
@@ -10,14 +10,52 @@ using namespace std;
 
 Link::Link(std::shared_ptr<cpptoml::table> config)
     : logger_(spdlog::get("deadeye")), serial_(nullptr) {
-  serial_.reset(new serial::Serial("/dev/ttyTHS2", 115200,
-                                   serial::Timeout::simpleTimeout(1000)));
+  assert(config);
+
+  string port = "/dev/null";
+  int speed = 115200;
+  int timeout = 1000;
+
+  auto link_config = config->get_table("LINK");
+  if (link_config) {
+    auto port_setting = link_config->get_as<string>("port");
+    if (port_setting) {
+      port = *port_setting;
+    } else {
+      logger_->warn("LINK port setting missing, using default");
+    }
+
+    auto speed_setting = link_config->get_as<int>("speed");
+    if (speed_setting) {
+      speed = *speed_setting;
+    } else {
+      logger_->warn("LINK port speed setting missing, using default");
+    }
+
+    auto timeout_setting = link_config->get_as<int>("timeout");
+    if (timeout_setting) {
+      timeout = *timeout_setting;
+    } else {
+      logger_->warn("LINK timeout setting missing, using default");
+    }
+  } else {
+    logger_->error("LINK configuration section missing, using defaults");
+  }
+  logger_->info("link on port {} at {} baud with {}ms timeout", port, speed,
+                timeout);
+
+  serial_.reset(
+      new serial::Serial(port, speed, serial::Timeout::simpleTimeout(timeout)));
 }
 
 /** Read serial line for current mode.
  */
 Link::Mode Link::GetMode() {
+#ifdef DEADEYE_TEST
+  return kQuitMode;
+#else
   return kBoilerMode;
+#endif
 }
 
 /** Send the boiler shooting solution across the serial line.
--- test/deadeye_test.cc
+++ test/deadeye_test.cc
@@ -1,6 +1,10 @@
 #include "catch/catch.hpp"
 #include "cpptoml/cpptoml.h"
 
+#include <cstdlib>
+#include <fcntl.h>
+#include <string>
+
 #include "deadeye.h"
 
 using namespace deadeye;
@@ -12,6 +16,16 @@ TEST_CASE("deadeye Run method", "[deadeye]") {
   camera->insert("width", 1280);
   camera->insert("height", 1080);
   camera->insert("exposure", -1.0);
+
+  // create a fake serial port out of a pseudo-tty
+  int pts = posix_openpt(O_RDWR | O_NOCTTY);
+  char* port = ttyname(pts);
+  REQUIRE(port != NULL);
+
+  auto link = cpptoml::make_table();
+  root->insert("LINK", link);
+  link->insert("port", std::string(port));
+
   Deadeye deadeye{root};
   REQUIRE_NOTHROW(deadeye.Run());
 }
