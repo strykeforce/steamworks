3ae6155de247d3fb88c3e75f49db6c7856642fb3
Jeff Hutchison <jeff@jeffhutchison.com>
2017-02-16 09:59:11 -0500

    Combine gyro and non-gyro swerve classes.
    The addition of SetGyroDisabled() made having the two classes with
    delegation superfluous.

--- examples/drivetest/subsystems/drive.h
+++ examples/drivetest/subsystems/drive.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "AHRS.h"
 #include "WPILib.h"
-#include "swerve/gyro_swerve_drive.h"
+#include "swerve/swerve_drive.h"
 
 namespace sidewinder {
 class Drive : public frc::Subsystem {
  private:
-  swerve::GyroSwerveDrive swerve_drive_;
+  std::shared_ptr<AHRS> gyro_;
+  swerve::SwerveDrive swerve_drive_;
 
  public:
   Drive(const std::shared_ptr<cpptoml::table> config);
