1c8cf109e83c9d8221950091f1689dce20b411be
Jeff Hutchison <jeff@jeffhutchison.com>
2017-03-10 16:45:04 -0600

    Update auton azimuth rotation distance.

--- src/commands/auton/sequence_04.cc
+++ src/commands/auton/sequence_04.cc
@@ -17,7 +17,7 @@ Sequence04::Sequence04() : frc::CommandGroup("Sequence04") {
   AddSequential(new drive::TimedSwerveDrive(-0.25, 0, 2.2));
 
   // azimuth
-  AddSequential(new drive::TimedAzimuth(0.4, 0.3));
+  AddSequential(new drive::TimedAzimuth(0.47, 0.3));
 
   // // start shooting
   AddSequential(new StartShooting());
