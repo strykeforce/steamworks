b4156ed6a4de4fd3f6d877061154311edfd8fa53
Jeff Hutchison <jeff@jeffhutchison.com>
2017-04-20 11:52:00 -0400

    Don't check for zero setpoint before resetting
    By checking for a zero setpoint, we would trigger a bug when setting
    speed for a close-shot twice in close succession. The second activation
    would detect that the wheel was driving its setpoint down to a stop and
    not reset its speed to the desired shooting speed.

--- src/commands/shooter/set_shooter.cc
+++ src/commands/shooter/set_shooter.cc
@@ -6,11 +6,6 @@
 
 using namespace steamworks::command::shooter;
 
-// namespace {
-// const double kTimeout = -1.0;
-// const int kElevationGoodEnough = 10;
-// }
-
 SetShooter::SetShooter(int speed, int elevation)
     : frc::InstantCommand("SetShooter"),
       logger_(spdlog::get("command")),
@@ -18,10 +13,8 @@ SetShooter::SetShooter(int speed, int elevation)
       elevation_(elevation) {}
 
 void SetShooter::Initialize() {
-  if (Robot::shooter_wheel->GetSpeedSetpoint() == 0) {
-    Robot::shooter_wheel->SetSpeed(speed_);
-    Robot::shooter_elevation->SetElevation(elevation_);
-    logger_->info("SetShooter initialized with speed {} and elevation {}",
-                  speed_, elevation_);
-  }
+  Robot::shooter_wheel->SetSpeed(speed_);
+  Robot::shooter_elevation->SetElevation(elevation_);
+  logger_->info("SetShooter initialized with speed {} and elevation {}", speed_,
+                elevation_);
 }
